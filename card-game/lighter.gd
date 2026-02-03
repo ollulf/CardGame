9bafd3294e743078d944b59ff957af3029bfd36c
ollulf <max.bredlau@googlemail.com>
2026-02-03 19:30:32 +0100

    ADDED: movement of ashtray and lighter

--- card-game/lighter.gd
+++ card-game/lighter.gd
@@ -1,40 +1,25 @@
 extends Area2D
-class_name CigPack
-
-@export var normal_texture: Texture2D
-@export var hover_texture: Texture2D
-@export var hover_offset: Vector2 = Vector2(0, -10)
-
-@export var cig_amount:= 10
-@onready var amount_label:= $Sprite2D/Amount
+class_name Lighter
+@export var hover_offset: Vector2 = Vector2(0, -4)
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 var _base_position: Vector2
 
 func _ready() -> void:
-	InteractionManager.cigPack = self
 	
 	_base_position = sprite.position
 
-	if normal_texture != null:
-		sprite.texture = normal_texture
-
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	
-	update_label()
+
 
 
 func _on_mouse_entered() -> void:
-	if hover_texture != null:
-		sprite.texture = hover_texture
 	sprite.position = _base_position + hover_offset
 
 
 func _on_mouse_exited() -> void:
-	if normal_texture != null:
-		sprite.texture = normal_texture
 	sprite.position = _base_position
 
 
@@ -46,9 +31,4 @@ func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> vo
 
 
 func on_clicked() -> void:
-	InteractionManager.pickedUpCig
-	cig_amount-= 1
-	update_label()
-
-func update_label():
-	amount_label.text = str(cig_amount);
+	InteractionManager.usedLighter()
